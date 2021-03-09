6fc8e4f2503a6a5622f353db46dc30b8e7a8bf97
nasfernane <nasfernane@gmail.com>
2021-03-09 16:38:00 +0100

    maj mineure

--- php/auth.php
+++ php/auth.php
@@ -11,7 +11,7 @@ $_SESSION['wrongLogin'] = false;
 // Connexion à la BDD et récupération des exceptions
 $host = 'mysql:host=mysql-nasfernane.alwaysdata.net;dbname=nasfernane_trollodb';
 try {
-    $tododb = new PDO($host, '216502', '');
+    $tododb = new PDO($host, '216502_nf', 'blabladodo1337');
     $tododb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
 // récupération des exceptions
